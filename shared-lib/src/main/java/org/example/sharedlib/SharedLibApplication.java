8dde9eceee6dbea51fdb6c0db31731de8c0cea18
joshua.odiwuor <joshua.odiwuor@turnkeyafrica.com>
2025-06-09 19:32:25 +0300

    Command Query Responsibility Using Kafka for data synchronization

--- shared-lib/src/main/java/org/example/sharedlib/SharedLibApplication.java
+++ shared-lib/src/main/java/org/example/sharedlib/SharedLibApplication.java
@@ -1,7 +1,6 @@
 package org.example.sharedlib;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
--- shared-lib/src/main/java/org/example/sharedlib/repository/EmployeeRepository.java
+++ shared-lib/src/main/java/org/example/sharedlib/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package org.example.sharedlib.repository;
 
 import org.example.sharedlib.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
 }
